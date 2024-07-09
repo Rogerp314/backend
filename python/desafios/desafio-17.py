4e21b7129ded636638dd524fa6c90c810a597f4c
Pedro Rogério <138505724+Rogerp314@users.noreply.github.com>
2024-07-09 15:48:17 -0300

    estudos

--- python/desafios/desafio-17.py
+++ python/desafios/desafio-17.py
@@ -1,5 +1,9 @@
-import math
+''' from math import trunc
 
 n = float(input('Digite qualquer número real: '))
-real = math.floor(n)
-print('A parte inteira do número {} é equivalente a {}.'.format(n, real))
+print('A parte inteira do número {} é equivalente a {}.'.format(n, trunc(n)))'''
+
+n = float(input('Digite um valor real qualquer: '))
+print('A parte inteira de {} vale {}.'.format(n, int(n)))
+
+#As linhas de códigos que não estão em comentário tem melhores resultados.
--- python/desafios/desafio-18.py
+++ python/desafios/desafio-18.py
@@ -4,3 +4,11 @@ adja = float(input('Qual o valor em cm do Cateto Adjacente do triângulo? '))
 opos = float(input('Qual o valor em cm do Cateto Oposto do triângulo? '))
 hip = math.hypot(adja, opos)
 print('O tamanho da hipotenusa é de {}cm.'.format(hip))
+
+
+#Outra maneira de se fazer o desafio é com o próprio conceito matemático.
+
+ad = float(input('Qual o comprimento do cateto adjacente? '))
+op = float(input('Qual o comprimento do cateto oposto? '))
+h = ((ad**2) + (op**2))**0.5
+print('A hipotenusa vale {}.'.format(h))
