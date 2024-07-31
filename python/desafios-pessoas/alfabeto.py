f6211b50ffe1151bf0446bbe641c782d660f2442
Pedro Rogério <138505724+Rogerp314@users.noreply.github.com>
2024-07-31 01:30:14 -0300

    Estudos

---
+++ python/desafios-pessoas/alfabeto.py
@@ -0,0 +1,7 @@
+#O desafio é que quando colocado um número entre 1 a 26 mostre o alfabeto do A até a letra de número escolhido
+alfa = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+num = int(input('Digite um número entre 1 e 26: '))
+print(*alfa[:num])
+cores = {'cor' : '\033[31m', 'limpa' : '\033[m'}
+if num == 0:
+    print('{} [ERROR] \nNão Existe letra no alfabeto que esteja na posição 0.{}'.format(cores['cor'], cores['limpa']))
