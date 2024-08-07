c180a599d10c32ccd726f0372330c1aff21fdb59
Pedro Rogério <138505724+Rogerp314@users.noreply.github.com>
2024-08-07 00:31:23 -0300

    Estudos

--- python/desafios/desafio-45.py
+++ python/desafios/desafio-45.py
@@ -2,15 +2,19 @@ preço = float(input('Digite o preço do produto:R$'))
 forma_pagamento = int(input(' Dinheiro/Pix(1) 10 porcento de desconto \n Cartão de crédito a vísta(2) 5 porcento de desconto \n Cartão de crédito em 2x (3) \n Cartão de crédito em 3x ou mais. Sujeito a juros de 20 porcento (4) \n Qual será a forma de pagamento?: '))
 
 vista = preço - (preço * 10/100)
-cartão_parcelado2 = preço - (preço * 5/100)
-cartão_pracelado3oumais = preço + (preço * 20/100)
+cartão_vista = preço - (preço * 5/100)
+cartão_parcelado3oumais = preço + (preço * 20/100)
 if forma_pagamento == 1:
     print('O Preço a ser pago no protudo é de R${:.2f}'.format(vista))
 elif forma_pagamento == 2:
-    print('O preço a ser pago pelo produto é de R${:.2f}'.format(cartão_parcelado2))
+    parcela = cartão_vista / 2
+    print('O preço a ser pago pelo produto é de R${:.2f}, o valor da parcela por mês é de R${:.2f}'.format(cartão_vista, parcela))
 elif forma_pagamento == 3:
-    print('O preço a ser pago pelo produto é de R${:.2f}'.format(preço))
+    parcela = preço / 2
+    print('O preço a ser pago pelo produto é de R${:.2f} ao todo, o valor por mês é de R${:.2f}'.format(preço, parcela))
 elif forma_pagamento == 4:
-    print('O preço a ser pago pelo produto é de R${:.2f}'.format(cartão_pracelado3oumais))
+    num_parcelas = int(input('Em quantas vezes será a parcela? '))
+    parcela = cartão_parcelado3oumais / num_parcelas
+    print('O preço a ser pago pelo produto é de R${:.2f} no total, por mês é de R${:.2f}'.format(cartão_parcelado3oumais, parcela))
 else:
     print('[ERROR] A opção escolhida não existe. Tente novamente.')
--- python/desafios/desafio-46.py
+++ python/desafios/desafio-46.py
@@ -3,11 +3,14 @@ from time import sleep
 
 input('Vamos jogar jokenpo com o computador? Pressione a tecla ENTER para começar.')
 lista = ['pedra', 'papel', 'tesoura']
-jogador = input('Diga qual você escolheu: ').lower()
+jogador = input('Diga qual você escolheu: ').strip().lower()
 pc = choice(lista)
-print('PROCESSANDO...')
-sleep(2)
-print('O computador escolheu {}'.format(pc.capitalize()))
+print('{:=^40}')#Corrigir problemas mais tarde!!!, antes de assistir o vídeo.
+print('JO...')
+sleep(1)
+print('KEN...')
+sleep(1)
+print('PÔ!!!')
 if pc == jogador:
     print('Vocês empataram.')
 elif pc == lista[0] and jogador == lista[1]:
@@ -16,7 +19,8 @@ elif pc == lista[1] and jogador == lista[2]:
     print('VOCÊ GANHOU!!!')
 elif pc == lista[2] and jogador == lista[0]:
     print('VOCÊ GANHOU!!!')
-elif jogador != 'pedra, papel, tesoura':
+elif jogador != lista[0] or jogador != lista[1] or jogador != lista[2]:
     print('\033[31m[ERROR]\033[m Escolha entre pedra, papel ou tesoura para jogar.')
 else:
     print('O computador ganhou. Tente novamente.')
+print('O computador escolheu {}'.format(pc.capitalize()))
